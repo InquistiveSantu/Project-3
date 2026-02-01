1d82d14d08b2e3bfe6fdc5ca0c85a3056edb86c9
pairasantu080@gmail.com <pairasantu080@gmail.com>
2026-02-02 01:22:21 +0530

    Initial Setup Done

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const express = require("express");
+const { users} = require("./data/users.json");
 
 const app = express();
 const PORT = 8081;
 
-// Middleware
 app.use(express.json());
 
-// Default route
+/**
+ * Home Route
+ */
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Server is up and running :-)",
+    data: "hey",
   });
 });
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+/**
+ * GET /users  → EXACT OUTPUT LIKE YOUR VIDEO
+ */
+app.get("/users", (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: users,
+  });
 });
+
+/**
+ * 404 Handler (Express 5 Safe)
+ */
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Route not found",
+  });
+});
+
+app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
